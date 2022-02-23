348ffeafabe15c392af55a907e0232e027d00c60
R00T80Y <R00T80Y@gmail.com>
2022-02-23 19:57:12 +0300

    ESLint: refactoring

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const PRODUCTION_MODE = process.env.NODE_ENV === "production";
+const PRODUCTION_MODE = process.env.NODE_ENV === 'production';
 
-let postCssPlugins = [
-  ["postcss-preset-env", {}],
-]
+const postCssPlugins = [
+  ['postcss-preset-env', {}]
+];
 
 if (PRODUCTION_MODE) {
-  postCssPlugins.push(["postcss-combine-media-query"]);
+  postCssPlugins.push(['postcss-combine-media-query']);
 }
 
 module.exports = {
-  'plugins': postCssPlugins,
+  plugins: postCssPlugins
 };
